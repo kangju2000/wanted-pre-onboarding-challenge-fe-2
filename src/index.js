b23737583a1435e53debe6dc0409e89bddda5b7e
kangju2000 <kangju2000@naver.com>
2023-02-03 13:39:59 +0900

    feat: Todo 앱 요구사항에 맞게 JSDoc을 활용하여 문서화 진행

--- src/index.js
+++ src/index.js
@@ -1,51 +1,159 @@
 /**
- * Represents a book.
- * @constructor
- * @param {string} title - The title of the book.
- * @param {string} author - The author of the book.
+ * Todo
+ * @typedef {Object} Todo
+ * @property {string} id - 아이디
+ * @property {string} content - 내용
+ * @property {boolean} done - 완료 여부
+ * @property {string} category - 카테고리
+ * @property {string[]} [tags] - 태그들
  */
-function Book(title, author) {}
 
 /**
- * Both of these will link to the bar function.
- * @see {@link bar}
- * @see bar
+ * Todo 생성
+ * @param {string} content - 내용
+ * @param {string} category - 카테고리
+ * @param {string[]} [tags] - 태그들
+ * @returns {Todo}
+ * @example
+ * const todo = CreateTodo('내용', '카테고리', ['태그1', '태그2']);
+ * console.log(todo);
+ * // {
+ * //   id: '1',
+ * //   content: '내용',
+ * //   done: false,
+ * //   category: '카테고리',
+ * //   tags: ['태그1', '태그2']
+ * // }
  */
-function foo() {}
-
-// Use the inline {@link} tag to include a link within a free-form description.
-/**
- * @see {@link foo} for further information.
- * @see {@link http://github.com|GitHub}
- */
-function bar() {}
-
-/**
- * Generic dairy product.
- * @constructor
- */
-function DairyProduct() {}
-
-/**
- * Check whether the dairy product is solid at room temperature.
- * @abstract
- * @return {boolean}
- */
-DairyProduct.prototype.isSolid = function () {
-	throw new Error('must be implemented by subclass!');
-};
+function CreateTodo(content, category, tags) {}
 
 /**
- * Cool, refreshing milk.
- * @constructor
- * @augments DairyProduct
+ * 특정 Todo 조회
+ * @param {string} id - 아이디
+ * @returns {Todo}
+ * @example
+ * const todo = GetTodo('1');
+ * console.log(todo);
+ * // {
+ * //   id: '1',
+ * //   content: '내용',
+ * //   done: false,
+ * //   category: '카테고리',
+ * //   tags: ['태그1', '태그2']
+ * // }
  */
-function Milk() {}
+function GetTodo(id) {}
 
 /**
- * Check whether milk is solid at room temperature.
- * @return {boolean} Always returns false.
+ * Todo 목록 조회
+ * @returns {Todo[]}
+ * @example
+ * const todos = GetTodos();
+ * console.log(todos);
+ * // [
+ * //   {
+ * //     id: '1',
+ * //     content: '내용',
+ * //     done: false,
+ * //     category: '카테고리',
+ * //     tags: ['태그1', '태그2']
+ * //   },
+ * //   {
+ * //     id: '2',
+ * //     content: '내용2',
+ * //     done: true,
+ * //     category: '카테고리',
+ * //     tags: []
+ * //   }
+ * // ]
  */
-Milk.prototype.isSolid = function () {
-	return false;
-};
+function GetTodos() {}
+
+/**
+ * Todo 수정
+ * @param {object} todo - Todo
+ * @param {string} todo.id - 아이디(수정 불가)
+ * @param {string} todo.content - 내용
+ * @param {boolean} todo.done - 완료 여부
+ * @param {string} todo.category - 카테고리
+ * @example
+ * const todo = GetTodo('1');
+ * const updatedTodo = UpdateTodo({
+ *  ...todo,
+ * done: true,
+ * });
+ * // 수정된 Todo
+ * // {
+ * //   id: '1',
+ * //   content: '내용',
+ * //   done: true,
+ * //   category: '카테고리',
+ * //   tags: ['태그1', '태그2']
+ * // }
+ */
+function UpdateTodo(todo) {}
+
+/**
+ * Todo 특정 태그 수정
+ * @param {string} id - 아이디
+ * @param {string} tag - 태그
+ * @example
+ * const todo = GetTodo('1');
+ * const updatedTodo = UpdateTagByTodoId('1', '태그3');
+ * // 수정된 Todo
+ * // {
+ * //   id: '1',
+ * //   content: '내용',
+ * //   done: false,
+ * //   category: '카테고리',
+ * //   tags: ['태그1', '태그2', '태그3']
+ * // }
+ */
+function UpdateTagByTodoId(id, tag) {}
+
+
+/**
+ * Todo 삭제
+ * @param {string} id - 아이디
+ */
+function DeleteTodo(id) {}
+
+/**
+ * Todo 전체 삭제
+ */
+function DeleteTodos() {}
+
+/**
+ * Todo 특정 태그 삭제
+ * @param {string} id - 아이디
+ * @param {string} tag - 태그
+ * @example
+ * const todo = GetTodo('1');
+ * const updatedTodo = DeleteTagByTodoId('1', '태그2');
+ * // 수정된 Todo
+ * // {
+ * //   id: '1',
+ * //   content: '내용',
+ * //   done: false,
+ * //   category: '카테고리',
+ * //   tags: ['태그1']
+ * // }
+ */
+function DeleteTagByTodoId(id, tag) {}
+
+/**
+ * Todo 특정 태그 전체 삭제
+ * @param {string} id - 아이디
+ * @example
+ * const todo = GetTodo('1');
+ * const updatedTodo = DeleteTagsByTodoId('1');
+ * // 수정된 Todo
+ * // {
+ * //   id: '1',
+ * //   content: '내용',
+ * //   done: false,
+ * //   category: '카테고리',
+ * //   tags: []
+ * // }
+ */
+function DeleteTagsByTodoId(id) {}
